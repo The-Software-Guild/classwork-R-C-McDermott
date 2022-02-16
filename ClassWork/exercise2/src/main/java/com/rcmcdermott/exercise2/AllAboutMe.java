7d6c890fcd76707f99d467a459accc4358a918d5
R-C-McDermott <ryan.c.mcdermott4@gmail.com>
2022-02-16 12:19:59 +0000

    exercise 2

---
+++ ClassWork/exercise2/src/main/java/com/rcmcdermott/exercise2/AllAboutMe.java
@@ -0,0 +1,22 @@
+
+package com.rcmcdermott.exercise2;
+
+
+public class AllAboutMe {
+    public static void main(String[] args) {
+        // Variables
+        String myName = "Ryan McDermott";
+        String favFood = "Spicy curry";
+        int numOfPets = 0;
+        String houseType = "flat";
+        boolean canWhistle = true;
+        
+        // Output
+        System.out.println("My name is " + myName + ".");
+        System.out.println("My favorite food is " + favFood + ".");
+        System.out.println("I have " + numOfPets + " pets.");
+        System.out.println("I live in a " + houseType + ", and I love it here!");
+        System.out.println("It is " + canWhistle + " I know how to whistle.");
+        
+    }
+}
